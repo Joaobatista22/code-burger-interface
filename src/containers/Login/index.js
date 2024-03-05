c4f6f391109a3263082c7ac7765568c6dc0faf6a
Joaobatista22 <joaobatiista.ds@gmail.com>
2024-03-05 20:21:30 -0300

    react-hook-form

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 import BurgerLogo from '../../assets/Login-Hamburguer.svg' // Renomeie o import para evitar conflito de nomes
 import Logo from '../../assets/Login-Text.svg'
@@ -14,6 +15,8 @@ import {
 } from './styles'
 
 function Login() {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = data => console.log(data)
   return (
     <Container>
       <LoginImage src={BurgerLogo} alt="Burger Logo" />
@@ -21,11 +24,13 @@ function Login() {
         <img src={Logo} alt="img logo" />
         <h1>Login</h1>
         <ContainerAcss>
-          <P>Email</P>
-          <Input />
-          <P>Senha</P>
-          <Input />
-          <Button>Entrar</Button>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <P>Email</P>
+            <Input type="email" {...register('email')} />
+            <P>Senha</P>
+            <Input type="password" {...register('password')} />
+            <Button type="submit">Entrar</Button>
+          </form>
           <SignInLink>
             Não possui conta?
             <a href="/cadastro"> Cadastrar-se</a>
--- src/containers/Login/styles.js
+++ src/containers/Login/styles.js
@@ -14,10 +14,14 @@ export const ContainerAcss = styled.div`
   justify-content: center;
   width: 20rem;
   gap: 3px;
+  form {
+    display: flex;
+    flex-direction: column;
+  }
 `
 
 export const ContainerItens = styled.div`
-  background: #373737;
+  background: #000000;
   width: 100%;
   padding: 25px 75px;
   display: flex;
@@ -43,10 +47,52 @@ export const P = styled.p`
 `
 
 export const Input = styled.input`
-  height: 26px;
+  height: 29px;
   border-radius: 4px;
+  padding-left: 7px;
+  border: none;
+  outline: none;
 `
 
-export const Button = styled.button``
+export const Button = styled.button`
+  margin-top: 10px;
+  height: 35px;
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  text-decoration: none;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  background: linear-gradient(to right, #8a2be2, #008000);
+  background-size: 200% 100%;
+  animation: gradientAnimation 5s ease infinite;
+  @keyframes gradientAnimation {
+    0% {
+      background-position: 0% 50%; /* Inicia o gradiente à esquerda */
+    }
+    50% {
+      background-position: 100% 50%; /* Move o gradiente para a direita */
+    }
+    100% {
+      background-position: 0% 50%; /* Retorna o gradiente à posição inicial */
+    }
+  }
+  &:active {
+    background: linear-gradient(
+      to right,
+      #008000,
+      #578622
+    ); /* Altera a cor de fundo ao clicar */
+  }
+  &:hover {
+    transform: scale(1.01);
+    transform-origin: bottom;
+  }
+`
 
-export const SignInLink = styled.p``
+export const SignInLink = styled.p`
+  color: #ffff;
+  margin-top: 5px;
+`
