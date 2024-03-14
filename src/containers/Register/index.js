01d7ffb787f8f63d62c785cdfdd2162ddbd17233
Joaobatista22 <joaobatiista.ds@gmail.com>
2024-03-14 20:00:37 -0300

    Register_confirm

--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -5,11 +5,11 @@ import { yupResolver } from '@hookform/resolvers/yup'
 
 import Button from '../../components/Button'
 import api from '../../services/api'
-import RegisterImg from '../../assets/register_img.svg' // Renomeie o import para evitar conflito de nomes
+import RegisterImg from '../../assets/register_img.svg'
 import Logo from '../../assets/Login-Text.svg'
 import {
   Container,
-  RegisterImage, // Renomeie o componente para evitar conflito de nomes
+  RegisterImage,
   ContainerItens,
   P,
   Perros,
@@ -20,6 +20,7 @@ import {
 
 function Register() {
   const schema = yup.object().shape({
+    name: yup.string().required('Nome é obrigatório'),
     email: yup
       .string()
       .email('Digite um e-mail válido!')
@@ -27,7 +28,12 @@ function Register() {
     password: yup
       .string()
       .required('Senha obrigatória!')
-      .min(6, 'A senha deve conter 6 digitos!')
+      .min(6, 'A senha deve conter 6 digitos!'),
+    confirmpassword: yup
+      .string()
+      .required('Senha obrigatória!')
+      .oneOf([yup.ref('password')], 'As senhas devem ser iguais!')
+      .min(6)
   })
   const {
     register,
@@ -37,7 +43,8 @@ function Register() {
     resolver: yupResolver(schema)
   })
   const onSubmit = async clientData => {
-    const response = await api.post('sessions', {
+    const response = await api.post('users', {
+      name: clientData.name,
       email: clientData.email,
       password: clientData.password
     })
@@ -46,12 +53,20 @@ function Register() {
 
   return (
     <Container>
-      <RegisterImage src={RegisterImg} alt="Register Logo" />
+      <RegisterImage src={RegisterImg} alt="Register Logo" />{' '}
+      {/* Mantido o uso do RegisterImage */}
       <ContainerItens>
         <img src={Logo} alt="img logo" />
-        <h1>Login</h1>
+        <h1>Cadastre-se</h1>
         <ContainerAcss>
           <form noValidate onSubmit={handleSubmit(onSubmit)}>
+            <P>Nome</P>
+            <Input
+              type="text"
+              {...register('name')}
+              error={errors.name?.message}
+            />
+            <Perros>{errors.name?.message}</Perros>
             <P>Email</P>
             <Input
               type="email"
@@ -66,13 +81,20 @@ function Register() {
               error={errors.password?.message}
             />
             <Perros>{errors.password?.message}</Perros>
+            <P>Confirmar Senha</P>
+            <Input
+              type="password"
+              {...register('confirmpassword')}
+              error={errors.confirmpassword?.message}
+            />
+            <Perros>{errors.confirmpassword?.message}</Perros>
             <Button type="submit" style={{ marginTop: 10 }}>
-              Entrar
+              Cadastrar
             </Button>
           </form>
           <SignInLink>
-            Não possui conta?
-            <a href="/cadastro"> Cadastrar-se</a>
+            Já possui conta?
+            <a href="/cadastro"> Entrar</a>
           </SignInLink>
         </ContainerAcss>
       </ContainerItens>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// import Login from './containers/Login'
-import Register from './containers/Register'
+import Login from './containers/Login'
+// import Register from './containers/Register'
 import GlobalStyles from './styles/globalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <>
-    <Register />
+    <Login />
     <GlobalStyles />
   </>
 )
